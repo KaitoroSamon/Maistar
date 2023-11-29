f29c82d582e3581679f3cf87083bb679676f45f0
Masato-Miyata <21c1020048mm@edu.tech.ac.jp>
2023-11-30 05:45:08 +0900

    宮田進捗色々

---
+++ Assets/member/Miyata/Scripts/EnemyMove.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyMove : MonoBehaviour
+{
+    public  float enemySpeed; 
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        Rigidbody2D rb = this.transform.GetComponent<Rigidbody2D>();
+        const float power = 20;
+        rb.AddForce(Vector3.right * ((enemySpeed - rb.velocity.x) * power));
+    }
+}
